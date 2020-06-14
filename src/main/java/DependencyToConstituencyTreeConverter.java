2c8cbccec088ae8134bdb49289d67b7e4b997710
oguzkeremyildiz <oguzkeremyildiz@gmail.com>
2020-06-14 14:25:16 +0300

    Initial Commit

--- src/main/java/DependencyToConstituencyTreeConverter.java
+++ src/main/java/DependencyToConstituencyTreeConverter.java
@@ -1,8 +1,6 @@
 import AnnotatedSentence.AnnotatedSentence;
-import AnnotatedTree.ParseTreeDrawable;
 import ParseTree.ParseTree;
 
 public interface DependencyToConstituencyTreeConverter {
     ParseTree convert(AnnotatedSentence annotatedSentence);
-    ParseTreeDrawable convertDrawable(AnnotatedSentence annotatedSentence);
 }
