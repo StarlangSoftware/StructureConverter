200788c5928bb2728fb282a6b7069351bd2d7aee
Olcay Taner YILDIZ <olcaytaner@isikun.edu.tr>
2020-07-04 17:29:36 +0300

    Added tests.

---
+++ src/test/java/DependencyToConstituencyTest.java
@@ -0,0 +1,23 @@
+import AnnotatedSentence.AnnotatedCorpus;
+import AnnotatedSentence.AnnotatedSentence;
+import AnnotatedTree.ParseTreeDrawable;
+import ParseTree.ParseTree;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import static org.junit.Assert.*;
+
+class DependencyToConstituencyTest {
+
+    @Test
+    void testConvert() throws FileNotFoundException {
+        SimpleDependencyToConstituencyTreeConverter simpleDependencyToConstituencyTreeConverter = new SimpleDependencyToConstituencyTreeConverter();
+        AnnotatedCorpus annotated = new AnnotatedCorpus(new File("train3"), ".train");
+        ParseTree tree1 = simpleDependencyToConstituencyTreeConverter.convert((AnnotatedSentence) annotated.getSentence(0));
+        ParseTree tree2 = new ParseTreeDrawable(new FileInputStream(new File("trainTrees/0000.train")));
+        assertEquals(tree1.toString(), tree2.toString());
+    }
+}
